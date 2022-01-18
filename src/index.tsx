cd5e6edfb71f56013efb0bbce9918dd61e492f5b
Josef Citrine <josef@citrine.dev>
2022-01-18 21:38:12 +0000

    Added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
